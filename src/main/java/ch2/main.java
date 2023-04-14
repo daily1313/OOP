9264296d0fa6da2a6547860e1af7ab50b4251b9d
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-04-14 18:26:33 +0900

    refactor : 생성한 view 클래스 적용

--- src/main/java/ch2/main.java
+++ src/main/java/ch2/main.java
@@ -10,16 +10,29 @@ public class main {
         Beverage beverage = new Beverage();
         Mushroom mushroom = new Mushroom();
         Door door = new Door();
+        OutputView outputView = new OutputView();
 
-        System.out.println("엘리스의 현재 키와 위치 ");
-        System.out.println("키 : "+ alice.getHeight() + " 위치 : "+alice.getNowLocation());
-
+        outputView.printAliceCurrentHeightAndLocation(alice);
+        System.out.println();
         while(!alice.getNowLocation().equals(Location.정원)) {
             alice.eatCake(10, cake);
+            outputView.printAliceCurrentHeight(alice);
+            outputView.printCurrentCakeQuantity(cake);
+            System.out.println();
             alice.eatMushroomFront(10, mushroom);
+            outputView.printAliceCurrentHeight(alice);
+            outputView.printCurrentMushRoomFrontQuantity(mushroom);
+            System.out.println();
             alice.eatMushroomBack(30, mushroom);
+            outputView.printAliceCurrentHeight(alice);
+            outputView.printCurrentMushRoomBackQuantity(mushroom);
+            System.out.println();
             alice.drinkBeverage(10, beverage);
+            outputView.printAliceCurrentHeight(alice);
+            outputView.printCurrentBeverageQuantity(beverage);
+            System.out.println();
             alice.move(door);
+            outputView.printAliceCurrentHeightAndDoorHeight(alice, door);
         }
         exit(0);
     }
