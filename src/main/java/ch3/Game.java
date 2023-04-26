3f284ea893d585136959d06a5b25e3aedbb91219
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-04-26 11:39:48 +0900

    refactor : print문 일부 수정

--- src/main/java/ch3/Game.java
+++ src/main/java/ch3/Game.java
@@ -15,9 +15,8 @@ import java.util.Scanner;
 public class Game {
 
     public static void main(String[] args) {
-
         System.out.println("게임을 시작합니다.");
-        System.out.println("원하는 캐릭터를 선정해주세요.(1:정원사, 2:왕, 3:여왕, 4:신하, 5 : 트럼프인간, 6:토끼)");
+        System.out.println("원하는 캐릭터를 선정해주세요.(1:정원사, 2:왕, 3:여왕, 4:신하, 5:병사, 6:트럼프인간, 7:토끼)");
         int optionNumber = chooseCharacterOptionNumber();
         playGame(optionNumber);
     }
@@ -29,7 +28,6 @@ public class Game {
     }
 
     private static void playGame(int characterOptionNumber) {
-
         if(characterOptionNumber == 1) {
             Trump gardener = Gardener.getInstance();
             gardener.act();
